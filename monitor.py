3edb6f661c92e20219a2b5d1d246c7307aee0460
WonXd2 <56256945+wonxd1337@users.noreply.github.com>
2026-07-06 17:02:04 +0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,11 +1,8 @@
 import time
-from adb_utils import is_app_running, start_app
+from adb_utils import is_app_running
 from delta_control import full_process
 
 def monitor_loop(packages, script_content, place_id, token, channel_id, interval=10):
-    """
-    Loop monitoring: cek setiap package, jika mati -> restart.
-    """
     while True:
         for pkg in packages:
             if not is_app_running(pkg):
